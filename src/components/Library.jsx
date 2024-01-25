ddca5c0bf529c609d0312e5e4389bcb7e8c07839
Joseph Harris <josephellisharris@gmail.com>
2024-01-25 11:43:14 +0000

    playlist section completed

--- src/components/Library.jsx
+++ src/components/Library.jsx
@@ -1,5 +1,6 @@
 import BookImage from '../assets/images/book.png'
 import PlusImage from '../assets/images/plus.png'
+import GlobeImage from '../assets/images/globe.png'
 
 
 function Library() {
@@ -9,6 +10,26 @@ function Library() {
         <p>Your Library</p>
         <img src={PlusImage} alt="plus image" className="plusImage" />
       <div className="playlistContainer">
+        <p style={{fontWeight: 'bold'}}>Create your first playlist</p>
+        <p>It's easy, we'll help you</p>
+        <button>Create Playlist</button>
+      </div>
+      <div className="podcastsContainer">
+        <p style={{fontWeight: 'bold'}}>Let's find some podcasts to follow</p>
+        <p>We'll keep you updated on new episodes</p>
+        <button>Browse products</button>
+      </div>
+      <div className="contactContainer">
+        <p>Legal</p>
+        <p>Privacy Center</p>
+        <p>Privacy Policy</p>
+        <p>Cookie Settings</p>
+        <p>About Ads</p>
+        <p>Accessibility</p>
+        <p>Cookies</p>
+        <div className="englishButton">
+        <button><img src={GlobeImage} alt="globe image"/>English</button>
+        </div>
       </div>
     </div>
   )
